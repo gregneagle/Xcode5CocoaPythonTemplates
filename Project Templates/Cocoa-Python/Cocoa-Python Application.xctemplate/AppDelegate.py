f51655099db7de06ebfdd5858503b09feaff3f7e
Greg Neagle <gregneagle@mac.com>
2014-04-30 07:07:27 -0700

    first commit

---
+++ Project Templates/Cocoa-Python/Cocoa-Python Application.xctemplate/AppDelegate.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+#  ___PROJECTNAMEASIDENTIFIER___AppDelegate.py
+#  ___PROJECTNAME___
+#
+#  Created by ___FULLUSERNAME___ on ___DATE___.
+#  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+#
+
+from Foundation import *
+from AppKit import *
+
+class ___VARIABLE_classPrefix:identifier___AppDelegate(NSObject):
+    def applicationDidFinishLaunching_(self, sender):
+        NSLog("Application did finish launching.")
---
+++ Project Templates/Cocoa-Python/Cocoa-Python Core Data Document-based Application.xctemplate/CocoaAppDocument.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+#  ___PROJECTNAMEASIDENTIFIER___Document.py
+#  ___PROJECTNAME___
+#
+#  Created by ___FULLUSERNAME___ on ___DATE___.
+#  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+#
+
+from Foundation import *
+from CoreData import *
+from AppKit import *
+
+class ___VARIABLE_classPrefix___Document(NSPersistentDocument):
+    def init(self):
+        self = super(___VARIABLE_classPrefix___Document, self).init()
+        # initialization code
+        return self
+        
+    def windowNibName(self):
+        return u"___VARIABLE_classPrefix___Document"
+    
+    def windowControllerDidLoadNib_(self, aController):
+        super(___VARIABLE_classPrefix___Document, self).windowControllerDidLoadNib_(aController)
+        # user interface preparation code
